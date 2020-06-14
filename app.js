416d0b17a202679775be288d728649f52cc2e235
wasimiqbal1 <wasim_iqbal@msn.com>
2020-06-14 17:51:49 +0500

    Updated

---
+++ app.js
@@ -0,0 +1,87 @@
+// ==== Chapter-1 & Task-1
+// alert("Welcome from Saylani Mass Training");
+
+// ==== Chapter-1 & Task-2
+//alert("Error! Please enter a valid password.");
+
+// ==== Chapter-1 & Task-3
+//alert("Welcome to JS Land...\nHappy Coding");
+
+// ==== Chapter-1 & Task-4
+//alert("Welcome to JS Land);
+//alert(Happy Coding!");
+
+// ==== Chapter-1 & Task-5
+//alert("Hello...I Can run through my web browser's console");
+
+// ==== Chapter-1 & Task-6 & 7
+//<title>Assignment JS</title>
+//  <script src="app.js"></script>
+//alert("Welcome from Saylani Mass Training");
+
+// ==== Chapter-2 & Task-1
+//var userName = "Wasim";
+
+// ==== Chapter-2 & Task-2
+//var myName = "Wasim Iqbal";
+//myName
+
+// ==== Chapter-2 & Task-3
+//var message = "Hello World";
+//alert(message);
+
+// ==== Chapter-2 & Task-4
+//var studentName = "Wasim Iqbal";
+//var studentAge = "35 years old";
+//var studentsubject = "Certified Mobile Application Development";
+//alert(studentName);
+//alert(studentAge);
+//alert(studentsubject);
+
+// ==== Chapter-2 & Task-5
+//alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// ==== Chapter-2 & Task-6
+//var email = "wasim_iqbal@msn.com"
+//alert("my email address is " + email);
+
+// ==== Chapter-3 & Task-1
+//var age = "39";
+//alert("I am " + age + " years old");
+
+// ==== Chapter-3 & Task-2
+//var n = ("10"); //number of visit
+//alert("You have visited the site " + n + " times");
+
+// ==== Chapter-3 & Task-3
+//var birthYear = "1981";
+//document.write("My Birth year is " + birthYear);
+
+
+// ==== Chapter-4 & Task-1
+//var firstName = "Wasim";
+//var lastName = "Iqbal";
+//var birthYear = "1981";
+
+// ==== Chapter-4 & Task-2
+//Legal Names
+/*var hello = 10;
+var _xyz = 20;
+var $work = 30;
+var user2 = 40;
+var i_info = 50;
+var my$work = 60;*/
+
+//Illegal Names
+/*var 2user = 10; 
+var my user = 20; 
+var hello#world = 30;
+var my-info = 40;
+var my?info = 50;
+var my*info = 50;*/
+
+// ==== Chapter-5 & Task-1
+/*var a = 3;
+var b = 5;
+var c = a + b;
+document.write("Sum of " + a + " and " + b + " is " + c);*/

bfb7dae426890226902d47b84a7e3b555f9714cc
wasimiqbal1 <wasim_iqbal@msn.com>
2020-06-21 13:01:20 +0500

    Update

--- app.js
+++ app.js
@@ -112,14 +112,35 @@ d = b % b;
 document.write("The reminder is : " + d);
 document.write("<br>");*/
 
+// ==== Chapter-5 & Task-4
+/*var ticketPrice = 600;
+var noOfTickets = 5;
+var cost = ticketPrice * noOfTickets;
+document.write("Total cost to buy " + noOfTickets + " tickets to a movie is " + cost + " PKR");*/
 
 
+// ==== Chapter-5 & Task-5
+/*var a = ("<h3>Table of 4:</h3>");
+document.write(a);
+for (var i = 1; i <= 10; i++) {
+    document.write("4 " + "x " + i + " = " + 4 * i + " <br> ");
+}*/
 
-// ==== Chapter-6-9 & Task-3
-/*var subject1 = prompt("Enter Subject 1");
-var subject2 = prompt("Enter Subject 2");
-var subject3 = prompt("Enter Subject 3");
+// ==== Chapter-5 & Task-6
+/*var celTemp = 37;
+var cnvtFarnht = (celTemp * 9 / 5) + 32;
+document.write(celTemp + " <sup>oC</sup> is " + cnvtFarnht + " <sup>oF</sup>" + " <br> ");
+var farenheitTemperature = 100;
+var convertCelcius = (farenheitTemperature - 32) * 5 / 9;
+document.write(farenheitTemperature + " <sup>oF</sup> " + convertCelcius + " <sup>oC</sup>  ");*/
 
-var total = 100;
+// ==== Chapter-5 & Task-7
+var heading = ("<h2>Shopping Cart</h2>");
+var priceItem1 = 650;
+var priceItem2 = 100;
+var orderQtyItem1 = 3;
+var orderQtyItem2 = 7;
+var shippingCharges = 100;
+var totalCost = (priceItem1 * orderQtyItem1) + (priceItem2 * orderQtyItem2) + shippingCharges
 
-var sub1obtained = ("Enter " + subject1 + "Marks")*/
+document.write(heading + " Price of item 1 is " + priceItem1 + " <br> " + " <br> " + "Quantity of item 1 is " + orderQtyItem1 + " <br> " + " <br> " + "Price of item 2 is " + priceItem2 + " <br> " + " <br> " + "Quantity of item 2 is " + orderQtyItem2 + " <br> " + " <br> " + "Shipping charges " + shippingCharges + " <br> " + " <br> " + "Total cost of your order is " + totalCost);
